a65dfed2586c2aae5fa9a76579042046892015f2
GoluffyxD <mhmkiran@gmail.com>
2020-09-01 13:21:58 +0530

    Added History Plot

---
+++ plotting.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# HISTORY
+# Using the history object returned from model.fit,
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
